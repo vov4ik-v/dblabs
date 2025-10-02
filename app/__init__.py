f89bc1cbb7846a047360e13bf83071f44570bc07
vov4ik-v <volodymyr.vasylyshyn05@gmail.com>
2025-10-02 11:48:28 +0300

    Test CD

--- app/__init__.py
+++ app/__init__.py
@@ -51,7 +51,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        return jsonify("Application is running."), 200
+        return jsonify("Application is running. Hello world"), 200
 
     return app
 
